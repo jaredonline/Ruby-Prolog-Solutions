d172576726623a5b62f1701274696261d4464ed0
Jared McFarland <jaredmcfarland@ubuntu.(none)>
2011-02-14 15:34:09 -0700

    first lesson

---
+++ 01.Lists/01.last_element.rb
@@ -0,0 +1,28 @@
+class LinkedList
+  
+  attr_accessor :next
+  attr_accessor :value
+  
+  def initialize(value)
+    @value = value
+    @next = nil
+  end
+  
+  def last_element
+    next_element = @next
+    last_element = self
+    while next_element != nil
+      last_element = next_element
+      next_element = next_element.next
+    end
+    
+    return last_element
+  end
+  
+end
+
+list = LinkedList.new(1)
+list.next = LinkedList.new(2)
+list.next.next = LinkedList.new(3)
+
+p list.last_element
